68482bde2ee7a93ca891b231a4180a4a88e641d1
Pieter Claus <pieter.claus1@gmail.com>
2025-01-18 18:10:00 +0100

    finish day 1 exercise. An app that takes user input and prints a combination of the city and the name of the pet.

---
+++ Day_1/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the bandname generator!")
+city = input("What is the city you grew up in?\n")
+name = input("What is the name of your animal?\n")
+
+print("Your bandname could be " + city + " " + name)
+
